749ad93e1f01cbaf0e33a62ddcde108a86b74222
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2020-05-28 23:57:50 -0700

    Update autoeval.py

--- autoeval.py
+++ autoeval.py
@@ -16,13 +16,13 @@ with open('./human_labels.csv') as csv_file:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-                reference = [row[0],row[1]]
-                candidate = [row[2]]
-                r.append([row[0].split(),row[1].split()])
-                c.append([row[2].split())
-                P_mul, R_mul, F_mul = score(candidate, reference, lang="en", rescale_with_baseline=True)
-                F_mul = F_mul.tolist()[0]
-                bs = bs+F_mul
+            reference = [row[0],row[1]]
+            candidate = [row[2]]
+            r.append([row[0].split(),row[1].split()])
+            c.append([row[2].split()])
+            P_mul, R_mul, F_mul = score(candidate, reference, lang="en", rescale_with_baseline=True)
+            F_mul = F_mul.tolist()[0]
+            bs = bs+F_mul
 print("BLEU1",corpus_bleu(r, c,weights=(1, 0, 0, 0)))
 print("BLEU2",corpus_bleu(r, c,weights=(0, 1, 0, 0)))
 print("BERTSCORE",float(bs)/150.0)
